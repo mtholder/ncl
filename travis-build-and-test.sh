0b4701d890193bc09c79e886e2f2fdac8311743a
Mark T. Holder <mtholder@gmail.com>
2015-01-30 14:28:05 +0100

    abs path for --prefix

--- travis-build-and-test.sh
+++ travis-build-and-test.sh
@@ -1,5 +1,5 @@
 autoreconf || exit
-CPPFLAGS="-D__extern_always_inline=inline" CC=clang CXX=clang++ ./configure --prefix=./installed --with-constfuncs || exit
+CPPFLAGS="-D__extern_always_inline=inline" CC=clang CXX=clang++ ./configure --prefix=$PWD/installed --with-constfuncs || exit
 make -j4 || exit 
 make check || exit
 make install || exit
