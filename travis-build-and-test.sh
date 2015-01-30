f104fa932137918162e81b01c0a0a7cd3cffd91a
Mark T. Holder <mtholder@gmail.com>
2015-01-30 14:18:07 +0100

    first attempt at getting NCL to be checked with Travis

---
+++ travis-build-and-test.sh
@@ -0,0 +1,6 @@
+autoreconf || exit
+CPPFLAGS="-D__extern_always_inline=inline" CC=clang CXX=clang++ ./configure --prefix=./installed --with-constfuncs || exit
+make -j4 || exit 
+make check || exit
+make install || exit
+make installcheck || exit
